a5000ce48fb64ab2afee5c43bbaf25c22e57de12
Guilherme Da Silva Barros <guilhermesilva986@gmail.com>
2018-04-13 10:37:24 -0300

    Bliblioteca do pacman

---
+++ upecman.h
@@ -0,0 +1,153 @@
+/***************************************************************************
+ *   upecman.h                                Version 20160529.013231      *
+ *                                                                         *
+ *   Pacman Ncurses                                                        *
+ *   Copyright (C) 2016         by Ruben Carlo Benante                     *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Ruben Carlo Benante                                                   *
+ *   Email: rcb@beco.cc                                                    *
+ *   Webpage: http://www.beco.cc                                           *
+ *   Phone: +55 (81) 3184-7555                                             *
+ ***************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/* definitions */
+
+#ifndef VERSION
+#define VERSION "20160529.013231" /**< Version Number */
+#endif
+
+/* Debug */
+#ifndef DEBUG /* gcc -DDEBUG=1 */
+#define DEBUG 0 /**< Activate/deactivate debug mode */
+#endif
+
+/** @brief Debug message if DEBUG on */
+#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
+
+/* limits */
+#define SBUFF 256 /**< String buffer */
+#define LABL 23 /**< Number of lines in labyrinth */
+#define LABC 21 /**< Number of columns in labyrinth */
+
+/* ---------------------------------------------------------------------- */
+/* globals */
+
+static int verb = 0; /**< verbose level, global within the file */
+
+const char labmodel[LABL][LABC] =
+{
+    /*01234567890123456789*/
+    {"####################"},    /* 0*/
+    {"#........##........#"},    /* 1*/
+    {"#o##.###.##.###.##o#"},    /* 2*/
+    {"#..................#"},    /* 3*/
+    {"#.##.#.######.#.##.#"},    /* 4*/
+    {"#....#...##...#....#"},    /* 5*/
+    {"####.###.##.###.####"},    /* 6*/
+    {"   #.#...B....#.#   "},    /* 7*/
+    {"   #.#.##--##.#.#   "},    /* 8*/
+    {"####.#.#  P #.#.####"},    /* 9*/
+    {"    ...#  I #...    "},    /*10*/
+    {"####.#.#  C #.#.####"},    /*11*/
+    {"   #.#.######.#.#   "},    /*12*/
+    {"   #.#........#.#   "},    /*13*/
+    {"####.#.######.#.####"},    /*14*/
+    {"#........##........#"},    /*15*/
+    {"#o##.###.##.###.##o#"},    /*16*/
+    {"#..#.....@......#..#"},    /*17*/
+    {"##.#.#.######.#.#.##"},    /*18*/
+    {"#....#...##...#....#"},    /*19*/
+    {"#.######.##.######.#"},    /*20*/
+    {"#..................#"},    /*21*/
+    {"####################"}     /*22*/
+};
+
+
+typedef enum e_ghostmode {chase, scatter, afraid, dead} t_ghostmode;
+typedef enum e_direction {up, left, down, right} t_direction;
+typedef enum e_ghosts {blinky, pinky, inky, clyde} t_ghosts;
+
+typedef struct st_position
+{
+    int y; /* line */
+    int x; /* column */
+} t_pos;
+
+typedef struct st_pacman
+{
+    t_pos pos; /* current pacman position */
+    t_direction dir; /* direction */
+    unsigned tenta; /* tentativa casa premov */
+    int life; /* how many lifes pacman has */
+    int score; /* dot = 10 point; pill = 50 points; ghost = 750 points; cherry = 500 */
+    int dots; /*variavel para armazenar numero de pac-dots para inicializacao dos fantasmas*/
+    int tdots; /*variavel para armazenar numero total de pac-dots comidos*/
+    int cdots; /*variavel para armazenar numero de dots para surgimento da cherry*/
+    int cont;
+    int fase; /*Numero da fase*/
+    time_t tInicio, tFim;
+    double tDecorrido;
+    int revert;
+    int mult;
+} t_pacman; 
+
+typedef struct st_ghost
+{
+    t_pos pos; /* ghost position */
+    t_direction dir; /* ghost current direction */
+    t_pos starget; /* ghost scatter preferred corner */
+    t_ghostmode mode; /* chase, scatter, afraid or dead */
+    time_t tInicio, tFim;
+    double tDecorrido;
+} t_ghost; 
+
+typedef struct st_pacdata
+{
+    char lab[LABL][LABC]; /* the labyrinth map */
+    t_pacman pacman; /* pacman data */
+    t_ghost ghost[4]; /* ghost[blinky], ghost[pinky], ghost[inky], ghost[clyde] */
+} t_game;
+
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+void help(void); /* print some help */
+void copyr(void); /* print version and copyright information */
+t_game upecman_init(void); /* initialize game variables */
+t_game p_init(t_game g); /* posicoes inciais - morte */
+t_game andap(t_game g, int kin); /* pacman movement */
+void printlab(t_game g); /* print the labyrinth */
+void printover(t_game g); /* print end */
+void coresfant(t_game g);/*print phant colors*/
+t_game score(t_game g); /* function to count points */
+double distpontos(int gy, int gx, t_pos alvo);
+/*fantasmas*/
+t_game fant(t_game g); /*funcao geral dos fantasmas*/
+t_game andablinky(t_game g); /*funcao de movimentacao do blinky*/
+t_game andainky(t_game g); /*funcao de movimentacao do inky*/
+t_game andapinky(t_game g); /*funcao de movimentacao do pinky*/
+t_game andaclyde(t_game g); /*funcao de movimentacao do clyde*/
+int breakgame(t_game g);
+t_game passfase(t_game g); /*funcao para trocar de fase*/
+t_game newfase(t_game g); /*funcao para reposicao de pontos e print da nova fase*/
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
